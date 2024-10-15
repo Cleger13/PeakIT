aa2ddfcd23235e1336125c85ae1db107dea37278
Eduard <114852880+Cleger13@users.noreply.github.com>
2024-10-16 01:39:56 +0900

    Added some files

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import app
+from kivy.uix.button import Button
+
+class Appliccation(App):
+    def build(self):
+        my_but = Button()
+        
+        return my_but
+    
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    Appliccation().run()
